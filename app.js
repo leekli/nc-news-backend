d1071c5dfe9ffa9ea30dd68e5e8483715fa338f3
leekli <li.kirkham@live.com>
2022-01-17 16:10:44 +0000

    Commit 17/01/2022: Error handling folder and file commited

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const { getTopics } = require("./controllers/topics.controllers.js");
 
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors,
+} = require("./errors/index.js");
+
 // Initalise express server
 const app = express();
 
@@ -11,32 +17,14 @@ app.use(express.json());
 // GET request Endpoints
 app.get("/api/topics", getTopics);
 
-// Error handling 1 - Error 404: Use app.all to capture all types of requests where the endpoint is invalid
+// Error handling - Error 404: Use app.all to capture all types of requests where the endpoint is invalid
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Invalid URL" });
 });
 
-// Error handling 2 - Error 400: If PSQL hits error code "22P02"
-app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad request" });
-  } else {
-    next(err);
-  }
-});
-
-// Error handling 3 - Dealing with errors by return status code
-app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
-
-// Error handling 4 - Any other error not accounted for above, return a Status error 500
-app.use((err, req, res, next) => {
-  res.status(500).send({ msg: "Internal server error" });
-});
+// Error handling
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
---
+++ errors/index.js
@@ -0,0 +1,22 @@
+// errors/index.js - Error handling file
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal server error" });
+};
