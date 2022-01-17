b40d65f70a0310f91dad5fc9f7d7f28f02ff3d73
leekli <li.kirkham@live.com>
2022-01-17 13:24:56 +0000

    commit 17/01/2022: inital setup and tables created in seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,59 @@
+const db = require("../connection.js");
+const format = require("pg-format");
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+  return db
+    .query(`DROP TABLE IF EXISTS articles;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS comments;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics;`);
+    })
+    .then(() => {
+      return db.query(`
+      CREATE TABLE topics (
+        slug TEXT NOT NULL,
+        description TEXT,
+        PRIMARY KEY(slug)
+      );`);
+    })
+    .then(() => {
+      return db.query(`
+      CREATE TABLE users (
+        username TEXT NOT NULL,
+        name TEXT NOT NULL,
+        avatar_url TEXT,
+        PRIMARY KEY(username)
+      );`);
+    })
+    .then(() => {
+      return db.query(`
+      CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        topic TEXT,
+        author TEXT,
+        body TEXT,
+        created_at DATE DEFAULT CURRENT_TIMESTAMP,
+        votes INT DEFAULT 0
+      );`);
+    })
+    .then(() => {
+      return db.query(`
+      CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY,
+        body TEXT,
+        votes INT DEFAULT 0,
+        author TEXT,
+        article_id INT,
+        created_at DATE DEFAULT CURRENT_TIMESTAMP
+      );`);
+    });
 };
 
 module.exports = seed;
---
+++ view-test-db.sql
@@ -0,0 +1,3 @@
+\c nc_news_test
+
+SELECT * FROM tablename;
