7e9c653d226df90cd90fed38628f2d2c93562d68
leekli <li.kirkham@live.com>
2022-01-19 08:47:56 +0000

    19/01/2022 Commit: Files commented, utils article and comment ID lookups added, and additional error handling tests added

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -27,3 +27,13 @@ exports.checkArticleExists = (article_id) => {
       }
     });
 };
+
+exports.checkTopicExists = (topic) => {
+  return db
+    .query("SELECT * FROM topics WHERE slug=$1", [topic])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+    });
+};
