f732669fca29a65b45e4bbea0c36b0add0a52c33
leekli <li.kirkham@live.com>
2023-05-09 12:01:18 +0100

    refactors /api

--- models/app.models.js
+++ models/app.models.js
@@ -1,16 +1,13 @@
-// models/app.models.js - Models file for dealing with requests from app.controllers.js
-
-const db = require("../db/connection.js");
 const fs = require("fs/promises");
 
 exports.fetchEndPoints = () => {
   return fs
     .readFile("./endpoints.json", "utf8")
-    .then((data) => {
-      const endpoints = JSON.parse(data);
-      return endpoints;
+    .then((fileContents) => {
+      const endPointsJson = JSON.parse(fileContents);
+      return endPointsJson;
     })
     .catch((err) => {
-      console.log("File does not exist!", err);
+      throw new Error("File does not exist", err);
     });
 };
