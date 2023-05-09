edebfd6ed8600e4187fe4b6ef70b598097ae3ecf
leekli <li.kirkham@live.com>
2023-05-09 15:31:19 +0100

    very tiny refactor to topics response controller file

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -3,7 +3,7 @@ const { fetchTopics, addTopic } = require("../models/topics.models.js");
 exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((topics) => {
-      res.status(200).send({ topics: topics });
+      res.status(200).send({ topics });
     })
     .catch((err) => {
       next(err);
