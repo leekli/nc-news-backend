15b58b674599eeeda3a5f70a4fd253eaf98a49ce
leekli <li.kirkham@live.com>
2023-05-10 15:42:52 +0100

    refactor utils file to async/await

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,20 +1,24 @@
 const db = require("../connection.js");
 const format = require("pg-format");
 
-exports.checkUserExists = (username) => {
-  return db
-    .query(`SELECT * FROM users WHERE username=$1`, [username])
-    .then(({ rows }) => {
-      if (rows.length) return true;
-      else return false;
-    });
+exports.checkUserExists = async (username) => {
+  const queryStr = format("SELECT * FROM users WHERE username = $1;");
+  const dbOutput = await db.query(queryStr, [username]);
+
+  if (dbOutput.rowslnegth) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 exports.checkExists = async (table, column, value) => {
   const queryStr = format("SELECT * FROM %I WHERE %I = $1;", table, column);
   const dbOutput = await db.query(queryStr, [value]);
 
-  if (dbOutput.rows.length === 0)
+  if (dbOutput.rows.length === 0) {
     return Promise.reject({ status: 404, msg: "Not found" });
-  else return true;
+  } else {
+    return true;
+  }
 };
