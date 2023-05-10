8849176c2067334a018d9b0cb820995b6dcdbdbd
leekli <li.kirkham@live.com>
2023-05-10 14:44:59 +0100

    refactors endpoint DELETE /api/comments/:comment_id

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,56 +1,38 @@
-// controllers/comments.controllers.js - Controllers file for dealing with requests from the 'comments' Router and 'comments' data table
-
 const {
   removeCommentById,
   updateCommentById,
   fetchCommentById,
 } = require("../models/comments.models.js");
 
-const { checkCommentExists } = require("../db/utils/utils.js");
+const { checkExists } = require("../db/utils/utils.js");
 
-// deleteCommentById function - Retrieves data from comments models file, and returns a status code of 204 and no data if successful to show delete request was successful
 exports.deleteCommentById = (req, res, next) => {
   const { comment_id } = req.params;
 
-  return checkCommentExists(comment_id)
-    .then((commentExists) => {
-      if (commentExists) {
-        return removeCommentById(comment_id).then((deletedComment) => {
-          res.status(204).send({});
-        });
-      } else {
-        return Promise.reject({ status: 404, msg: "Not found" });
-      }
+  return checkExists("comments", "comment_id", comment_id)
+    .then(() => {
+      return removeCommentById(comment_id).then(() => {
+        res.sendStatus(204);
+      });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 };
 
-// patchCommentById function - Retrieves data from articles models file, and returns a status code of 200 and the data if successful
 exports.patchCommentById = (req, res, next) => {
   const { comment_id } = req.params;
-  const comment = req.body;
+  const commentBody = req.body;
 
-  return checkCommentExists(comment_id)
-    .then((commentExists) => {
-      if (commentExists) {
-        if (Object.keys(comment).length !== 0) {
-          return updateCommentById(comment_id, req.body).then(
-            (updatedComment) => {
-              res.status(200).send({ comment: updatedComment });
-            }
-          );
-        } else if (Object.keys(comment).length === 0) {
-          return fetchCommentById(comment_id).then((requestedComment) => {
-            res.status(200).send({ comment: requestedComment });
-          });
-        }
-      } else {
-        return Promise.reject({ status: 404, msg: "Not found" });
+  return checkExists("comments", "comment_id", comment_id)
+    .then(() => {
+      if (Object.keys(commentBody).length !== 0) {
+        return updateCommentById(comment_id, commentBody).then((comment) => {
+          res.status(200).send({ comment });
+        });
+      } else if (Object.keys(commentBody).length === 0) {
+        return fetchCommentById(comment_id).then((comment) => {
+          res.status(200).send({ comment });
+        });
       }
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 };
